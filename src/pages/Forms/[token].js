11f18006fe8df5b92d2c88bfdc2b334bc2dbd4b8
Raul <castillosteven09@outlook.es>
2023-12-31 15:01:55 -0500

    FrontedUsuarioForm

--- src/pages/Forms/[token].js
+++ src/pages/Forms/[token].js
@@ -1,13 +1,20 @@
 import React from 'react'
 import { useRouter } from 'next/router';
+//importar la barra de navegacion 
+import { NavBarForms } from '@/components/FormsLayout'
 
 export default function Formulario() {
     const router = useRouter();
     const { token } = router.query;
 
     //primero verificar si el formulario esta activo y mostrar el login xd
-
+    //para realizar la consulta sobre el estado del formulario se obtiene el token desde la URL
     return (
-        <div>token: {token}</div>
+        <div>
+            <div>
+                <NavBarForms user_name={"Hola"} titulo={"Inicio"} />
+            </div>
+            token: {token}
+        </div>
     )
 }

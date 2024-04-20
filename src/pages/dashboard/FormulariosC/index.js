3d429bd60e3c02178644d21d87adfeec73a8d863
Rip derek <castillosteven09@outlook.es>
2024-04-20 00:22:01 -0500

    NumPreguntasXnivel

--- src/pages/dashboard/FormulariosC/index.js
+++ src/pages/dashboard/FormulariosC/index.js
@@ -1,18 +1,18 @@
-export { default as Lista } from '@/pages/dashboard/FormulariosC/Lista'
-export { default as Crear } from '@/pages/dashboard/FormulariosC/Crear'
-export { default as Errores_Test } from '@/pages/dashboard/FormulariosC/Errores_Test'
-export { default as Participantes } from '@/pages/dashboard/FormulariosC/Participantes'
-export { default as SeleccionarSecciones } from '@/pages/dashboard/FormulariosC/SeleccionarSecciones'
-export { default as AddSeccion } from '@/pages/dashboard/FormulariosC/AddSeccion'
-export { default as SeccionesDisponibles } from '@/pages/dashboard/FormulariosC/SeccionesDisponibles'
-export { default as ListaPregunta } from '@/pages/dashboard/FormulariosC/ListaPregunta'
-export { default as OpcionesParticipantes } from '@/pages/dashboard/FormulariosC/OpcionesParticipantes'
-export { default as IngresosUsuario } from '@/pages/dashboard/FormulariosC/IngresosUsuario'
-export { default as ProgresoUsuario } from '@/pages/dashboard/FormulariosC/ProgresoUsuario'
-export { default as ProgresoSecciones } from '@/pages/dashboard/FormulariosC/ProgresoSecciones'
-export { default as ProgresoPregunta } from '@/pages/dashboard/FormulariosC/ProgresoPregunta'
-export { default as EliminarParticipanteTest } from '@/pages/dashboard/FormulariosC/EliminarParticipanteTest'
-export { default as ListaProgreso } from '@/pages/dashboard/FormulariosC/ListaProgreso'
+export { default as Lista } from "@/pages/dashboard/FormulariosC/Lista";
+export { default as Crear } from "@/pages/dashboard/FormulariosC/Crear";
+export { default as Errores_Test } from "@/pages/dashboard/FormulariosC/Errores_Test";
+export { default as Participantes } from "@/pages/dashboard/FormulariosC/Participantes";
+export { default as SeleccionarSecciones } from "@/pages/dashboard/FormulariosC/SeleccionarSecciones";
+export { default as AddSeccion } from "@/pages/dashboard/FormulariosC/AddSeccion";
+export { default as SeccionesDisponibles } from "@/pages/dashboard/FormulariosC/SeccionesDisponibles";
+export { default as ListaPregunta } from "@/pages/dashboard/FormulariosC/ListaPregunta";
+export { default as OpcionesParticipantes } from "@/pages/dashboard/FormulariosC/OpcionesParticipantes";
+export { default as IngresosUsuario } from "@/pages/dashboard/FormulariosC/IngresosUsuario";
+export { default as ProgresoUsuario } from "@/pages/dashboard/FormulariosC/ProgresoUsuario";
+export { default as ProgresoSecciones } from "@/pages/dashboard/FormulariosC/ProgresoSecciones";
+export { default as ProgresoPregunta } from "@/pages/dashboard/FormulariosC/ProgresoPregunta";
+export { default as EliminarParticipanteTest } from "@/pages/dashboard/FormulariosC/EliminarParticipanteTest";
+export { default as ListaProgreso } from "@/pages/dashboard/FormulariosC/ListaProgreso";
+export { default as EditarNivelesSeccion } from "@/pages/dashboard/FormulariosC/EditarNivelesSeccion";
 
-
-//ListaProgreso
+//EditarNivelesSeccion
